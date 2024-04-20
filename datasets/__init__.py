fix bug that normalization does not take into account mask

datasets/__init__.py
@@ -52,9 +52,12 @@ def read_dataset(
     conditioning = np.array(conditioning[conditioning_parameters].values)
 
     # Standardize per-feature (over datasets and particles)
+    # TODO: This needs to take into account the mask
+    # Will required re-training the existing models
     if norm_dict is None:
-        x_mean = x.mean(axis=(0, 1))
-        x_std = x.std(axis=(0, 1))
+        mask_bool = mask.astype(bool)
+        x_mean = x.mean(axis=(0, 1), where=mask_bool[..., None])
+        x_std = x.std(axis=(0, 1), where=mask_bool[..., None])
         cond_mean = conditioning.mean(axis=0)
         cond_std = conditioning.std(axis=0)
         norm_dict = {